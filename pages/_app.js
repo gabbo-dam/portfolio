b9e106a9770267dd97b138e4918faf5d0e6684af
Gabriel Morales <gabbomorales1@gmail.com>
2021-01-05 12:53:01 -0600

    Fixing some routing problems

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Theme from '@styles/Theme'
 import GlobalStyles from '@styles/GlobalStyles'
-import Layout from '../layout'
+import Layout from '@organisms/Layout'
 import { ThemeProvider } from 'styled-components'
 
 export default function App({ Component, pageProps }) {
