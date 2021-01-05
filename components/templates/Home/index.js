e568feefdcf8965d3513c7b164020de200f5fbc0
Gabriel Morales <gabbomorales1@gmail.com>
2021-01-05 12:50:11 -0600

    Testing Images

--- components/templates/Home/index.js
+++ components/templates/Home/index.js
@@ -1,10 +1,7 @@
 import { StyledHomePageMainContent, Container } from './styles'
 
-import Text from '@atoms/Text'
 import Project from '@molecules/Project'
 import Header from '@organisms/Header'
-import StandardTextHolder from '@molecules/StandardTextHolder'
-import Anchor from '@atoms/Anchor'
 
 const Home = () => {
   return (
@@ -15,36 +12,42 @@ const Home = () => {
         <Project
           href="https://damgamingclub.com/"
           src="/images/damgamingclub.PNG"
+          alt="Dam Gaming Club"
         >
           Dam Gaming Club
         </Project>
         <Project
           href="https://mystifying-jackson-735ff0.netlify.app"
           src="/images/Opera Snapshot_2021-01-05_101129_mystifying-jackson-735ff0.netlify.app.png"
+          alt="Coffee Blog"
         >
-          Coffe Blog
+          Coffee Blog
         </Project>
         <Project
           href="https://lucid-hopper-d05210.netlify.app"
           src="/images/Opera Snapshot_2021-01-05_080827_lucid-hopper-d05210.netlify.app.png"
+          alt="Real State Website"
         >
           Real State Website
         </Project>
         <Project
           href="https://angry-swanson-ff3e45.netlify.app"
           src="/images/Opera Snapshot_2020-12-17_125627_angry-swanson-ff3e45.netlify.app.png"
+          alt="Simon Says"
         >
           Simon Says
         </Project>
         <Project
           href="https://platzi-badges-wheat.vercel.app"
           src="/images/Home.png"
+          alt="Platzi Badges"
         >
           Platzi Badges
         </Project>
         <Project
           href="https://eloquent-johnson-0bb2b2.netlify.app"
           src="/images/Opera Snapshot_2021-01-05_101026_eloquent-johnson-0bb2b2.netlify.app.png"
+          alt="Traveling Blog"
         >
           Traveling Blog
         </Project>
